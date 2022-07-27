9b50d34c6e4352a252624f64f856b763658ae59f
Rob Schlereth <robert.schlereth@gmail.com>
2022-07-27 11:01:37 -0500

    Test: Create it exists test

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -3,4 +3,7 @@ require './lib/patron'
 require './lib/exhibit'
 
 RSpec.describe Museum do
-    it 'exists'
+    it 'exists' do
+        dmns = Museum.new("Denver Museum of Nature and Science")
+        expect(dmns).to be_instance_of(Museum)
+    end
