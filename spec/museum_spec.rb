3ec1ba4230dad6199f21ab262a8845b9571ca662
Rob Schlereth <robert.schlereth@gmail.com>
2022-07-27 11:10:50 -0500

    Test: Create added interests test

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -17,3 +17,25 @@ RSpec.describe Museum do
         dmns = Museum.new("Denver Museum of Nature and Science")
         expect(dmns.exhibits).to eq ([])
     end
+
+    it 'can add exhibits'
+        dmns = Museum.new("Denver Museum of Nature and Science")
+        gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+        dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+        imax = Exhibit.new({name: "IMAX",cost: 15})
+
+        dmns.add_exhibit(gems_and_minerals)
+        dmns.add_exhibit(dead_sea_scrolls)
+        dmns.add_exhibit(imax)
+    end
+
+    it 'has added interests' do
+        dmns = Museum.new("Denver Museum of Nature and Science")
+        gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+        dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+        imax = Exhibit.new({name: "IMAX",cost: 15})
+        patron_1 = Patron.new("Bob", 20)
+
+        patron_1.add_interest("Dead Sea Scrolls") 
+        patron_1.add_interest("Gems and Minerals")
+end
