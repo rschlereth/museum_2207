d33f9d8439e3ac2e46718630eb4a6ca989248b1a
Rob Schlereth <robert.schlereth@gmail.com>
2022-07-27 10:15:02 -0500

    Test: Create it has a name test

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -4,8 +4,11 @@ require './lib/patron'
 RSpec.describe Patron do
     it 'exists' do
         patron_1 = Patron.new("Bob", 20)
-        expect(patron).to be_instance_of(Patron)
+        expect(patron_1).to be_instance_of(Patron)
     end
 
-    it
+    it 'has a name' do
+        patron_1 = Patron.new("Bob", 20)
+        expect(patron_1.name).to eq("Bob")
+    end
 end    
