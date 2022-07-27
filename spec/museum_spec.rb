1ee1891d4d3616a1afa53d5fa30f8317a581990c
Rob Schlereth <robert.schlereth@gmail.com>
2022-07-27 11:13:18 -0500

    Test: Create add patrons test

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -38,4 +38,15 @@ RSpec.describe Museum do
 
         patron_1.add_interest("Dead Sea Scrolls") 
         patron_1.add_interest("Gems and Minerals")
+
+    it 'can add patrons' do
+        dmns = Museum.new("Denver Museum of Nature and Science")
+        gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+        dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+        imax = Exhibit.new({name: "IMAX",cost: 15})
+        patron_1 = Patron.new("Bob", 20)
+        patron_2 = Patron.new("Sally", 20)
+
+        patron_2.add_interest("IMAX")
+    end
 end
